929ce3e1fc039ab9afe0ec7c96b66a897d999459
markmcsweeney <markmcsweeney@users.noreply.github.com>
2019-12-04 10:39:32 -0500

    Update esxiX-certcheck.sh

--- esxiX-certcheck.sh
+++ esxiX-certcheck.sh
@@ -15,12 +15,13 @@ ESXICERT=$(date -d "`echo | openssl s_client -servername ${IP} -connect ${IP}:44
 if [ "$LOCALCERT" == "$ESXICERT" ]; then
   echo "Cert for $ESXI not due for renewal today `date '+%Y-%m-%d'`." >> /var/log/cert/renew.log
 
+## Changed 'services.sh restart to only '/etc/init.d/hostd restart' due to error: "Invalid operation requested: This ruleset is required and cannot be disabled" on ESXi 6.5 and >.
+
   else
    scp -i PATHTOSSHKEY /etc/letsencrypt/live/"$ESXI"."$DOMAINNAME"/fullchain.pem root@"$IP":/etc/vmware/ssl/rui.crt | \
    scp -i PATHTOSSHKEY -p /etc/letsencrypt/live/"$ESXI"."$DOMAINNAME"/privkey.pem root@"$IP":/etc/vmware/ssl/rui.key | \
    ##ssh -i PATHTOSSHKEY root@"$IP" 'services.sh restart' > /dev/null 2>&1 | \
    ssh -i PATHTOSSHKEY root@"$IP" '/etc/init.d/hostd restart' > /dev/null 2>&1 | \
-   ssh -i PATHTOSSHKEY root@"$IP" '/etc/init.d/vpxa restart' > /dev/null 2>&1 | \
    echo "SSL Certificate for $ESXI.$DOMAINNAME was updated on "`date '+%Y-%m-%d'`' at '`date '+%R %Z'`'.' | \
    mail -s "SSL Certificate for $ESXI.$DOMAINNAME has been updated" SETEMAILHERE -aFrom:'SET FROM EMAIL NAME HERE<SETFROMEMAILHERE>'
 
